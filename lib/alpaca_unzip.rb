6cd54b61fa3fb6155ae4c9caedfe20f8ebe4bcbe
alpaca-tc <alpaca-tc@alpaca.tc>
2019-08-12 16:16:15 +0900

    Initialize repository

---
+++ lib/alpaca_unzip.rb
@@ -0,0 +1,6 @@
+require "alpaca_unzip/version"
+
+module AlpacaUnzip
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/alpaca_unzip/version.rb
@@ -0,0 +1,3 @@
+module AlpacaUnzip
+  VERSION = "0.1.0"
+end
