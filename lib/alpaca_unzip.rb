236c8d7380c2051638707648bcde14892ab5aea6
alpaca-tc <alpaca-tc@alpaca.tc>
2019-08-12 21:41:52 +0900

    Add TODO

--- lib/alpaca_unzip.rb
+++ lib/alpaca_unzip.rb
@@ -3,5 +3,6 @@
 require 'alpaca_unzip/version'
 
 module AlpacaUnzip
+  # TODO: Replace the follows code with `autoload` or `require`
   Dir[File.expand_path('alpaca_unzip/**/*', __dir__)].each { |path| require(path) }
 end
