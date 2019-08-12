3e879fa1716268603992bfdadc21ed25e128c882
alpaca-tc <alpaca-tc@alpaca.tc>
2019-08-12 16:19:11 +0900

    Install dependencies

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in alpaca_unzip.gemspec
 gemspec
+
+gem 'pry'
+gem 'rubocop'
+gem 'rspec'
+gem 'guard'
+gem 'guard-rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,37 @@
-require "bundler/setup"
-require "alpaca_unzip"
+# frozen_string_literal: true
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.filter_run_when_matching :focus
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  config.profile_examples = 10
+
+  config.order = :random
+
+  config.filter_gems_from_backtrace(
+    'bootsnap',
+    'spring-commands-rspec'
+  )
+
+  # Guard + springを使うと、seed値が固定されてしまう問題を修正
+  # Randomize seed every time
+  config.seed = srand % 0xFFFF unless ARGV.any? { |arg| arg =~ /seed/ }
+
+  Kernel.srand config.seed
 end
